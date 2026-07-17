03abcaa1f106461bdba0c0f23ad0eb5b621c586f
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2026-07-17 09:15:09 -0400

    Rename 'df' to 'soil_temp15' for consistency and clarity

--- Analysis_Script.R
+++ Analysis_Script.R
@@ -9,22 +9,22 @@ library(patchwork)
 
 # Read in data from soil temp parquet file 
 file_path <- "TMP_F_2025_soil-temp-15cm_L2_v2-1.parquet"
-df <- read_parquet(file_path)
+soil_temp15 <- read_parquet(file_path)
 
 #Summarizing 
-head(df)
-names(df)
-dim(df)
-str(df)
-print(colnames(df))
-summary(df)
+head(soil_temp15)
+names(soil_temp15)
+dim(soil_temp15)
+str(soil_temp15)
+print(colnames(soil_temp15))
+summary(soil_temp15)
 
 #removing columns 
-df <- df |> select (-Site, -Instrument, -research_name)
+soil_temp15 <- soil_temp15 |> select (-Site, -Instrument, -research_name)
 
 # Line Chart of all the data
 # Randomly sub_sample the data to make plotting faster
-df |>
+soil_temp15 |>
   slice_sample(n = 10000) |>
   ggplot(aes(x = TIMESTAMP, y = Value)) +
   geom_point(color = "pink") + 
@@ -33,7 +33,7 @@ df |>
 
 #group by month and sensor
 #take the mean of the value column
-df |> 
+soil_temp15 |> 
   mutate(Month = month(TIMESTAMP)) |> 
   group_by(Month, Sensor_ID) |>                             
   summarize(mean_temp = mean(Value, na.rm = TRUE)) |>       
@@ -43,7 +43,7 @@ df |>
 
 
 #boxplot for all of data
-df |> 
+soil_temp15 |> 
   mutate(Month = month(TIMESTAMP)) |> 
   group_by(Month) |> 
   summarize(mean_temp = mean(Value, na.rm = TRUE)) |> 
@@ -51,7 +51,7 @@ df |>
   geom_boxplot()
 
 
-df |> 
+soil_temp15 |> 
        mutate(Month = month(TIMESTAMP)) |> 
        group_by(Month) |> 
        summarize(mean_temp = mean(Value, na.rm = TRUE)) |> 
@@ -59,24 +59,24 @@ df |>
        geom_col()
 
 #convert to date type and extract the month
-df<- df |> 
+soil_temp15<- soil_temp15 |> 
   mutate(
     month_column = as.Date(TIMESTAMP)
   )
-df <- df |> 
+soil_temp15 <- soil_temp15 |> 
   mutate(month = month(month_column, label = TRUE))
 
 
 #Boxplot
 ggplot(
-  df, aes(x = month, y = Value, group = month, fill = as.factor(month))) + 
+  soil_temp15, aes(x = month, y = Value, group = month, fill = as.factor(month))) + 
   geom_boxplot() + 
   labs(litle = "By Month", x = "Month", y = "Temperature") + 
   theme_minimal() + theme(asix.text.x = element_text(angle = 45, hjust= 1), 
   legend.position = "none")
 
 #Box plot with colors 
-df |> 
+soil_temp15 |> 
   slice_sample(n = 1000) |> 
   ggplot(aes(x = month, y = Value, color = Value)) + 
   geom_point(na.rm = TRUE) +
@@ -87,7 +87,7 @@ df |>
 
 #separate  
 ggplot(
-  df, aes(x = month, y = Value)) + 
+  soil_temp15, aes(x = month, y = Value)) + 
   geom_boxplot() + 
   facet_wrap(~ month, scales = "free_x", ncol = 6) + 
   labs(title = "Temperature by Month", y = "Temperature") + 
@@ -95,73 +95,73 @@ ggplot(
 
 #patchwork and combining
 m1 <- ggplot(
-  filter(df, month == "Jan"), 
+  filter(soil_temp15, month == "Jan"), 
   aes( x = month, y = Value)) + 
   geom_boxplot() + 
   labs(title= "January")
 
 m2 <- ggplot(
-  filter(df, month == "Feb"), 
+  filter(soil_temp15, month == "Feb"), 
   aes( x = month, y = Value)) + 
   geom_boxplot() + 
   labs(title= "February")
 
 m3 <- ggplot(
-   filter(df, month == "Mar"), 
+   filter(soil_temp15, month == "Mar"), 
    aes( x = month, y = Value)) + 
    geom_boxplot() + 
   labs(title= "March")
 
 m4 <- ggplot(
-         filter(df, month == "Apr"), 
+         filter(soil_temp15, month == "Apr"), 
             aes( x = month, y = Value)) + 
             geom_boxplot() + 
             labs(title= "April")
 
 m5 <- ggplot(
-         filter(df, month == "May"), 
+         filter(soil_temp15, month == "May"), 
             aes( x = month, y = Value)) + 
             geom_boxplot() + 
             labs(title= "May")
 
 m6 <- ggplot(
-         filter(df, month == "Jun"), 
+         filter(soil_temp15, month == "Jun"), 
             aes( x = month, y = Value)) + 
             geom_boxplot() + 
             labs(title= "June")
 
 m7 <- ggplot(
-         filter(df, month == "Jul"), 
+         filter(soil_temp15, month == "Jul"), 
             aes( x = month, y = Value)) + 
             geom_boxplot() + 
             labs(title= "July")
 
 m8 <- ggplot(
-         filter(df, month == "Aug"), 
+         filter(soil_temp15, month == "Aug"), 
             aes( x = month, y = Value)) + 
             geom_boxplot() + 
             labs(title= "August")
 
 m9 <- ggplot(
-         filter(df, month == "Sep"), 
+         filter(soil_temp15, month == "Sep"), 
            aes( x = month, y = Value)) + 
         geom_boxplot() + 
            labs(title= "September")
 
 m10 <- ggplot(
-         filter(df, month == "Oct"), 
+         filter(soil_temp15, month == "Oct"), 
             aes( x = month, y = Value)) + 
             geom_boxplot() + 
             labs(title= "October")
 
 m11 <- ggplot(
-         filter(df, month == "Nov"), 
+         filter(soil_temp15, month == "Nov"), 
             aes( x = month, y = Value)) + 
             geom_boxplot() + 
             labs(title= "November")
 
 m12 <- ggplot(
-         filter(df, month == "Dec"), 
+         filter(soil_temp15, month == "Dec"), 
            aes( x = month, y = Value)) + 
             geom_boxplot() + 
             labs(title= "December")
@@ -172,7 +172,7 @@ m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + m10 + m11 + m12 +
 
 #per-month variability 
 
-df |> 
+soil_temp15 |> 
   group_by(month) |>
   # compute mean and s.d. for each month
   summarise(mean_value = mean(Value, na.rm = TRUE), 
@@ -219,12 +219,12 @@ soil_vwc |>
 
 #merged
 soil_vwc <- soil_vwc |> select (-Instrument, -Instrument_ID, -Location, -Value_MAC)
-df <- df |> select (-Plot, -Instrument_ID, -Location, -N_avg, - N_drop, -Value_MAC)
-df <- rename(df, TimeStamp_temp = TIMESTAMP, Value_temp = Value)
+soil_temp15 <- soil_temp15 |> select (-Plot, -Instrument_ID, -Location, -N_avg, - N_drop, -Value_MAC)
+soil_temp15 <- rename(soil_temp15, TimeStamp_temp = TIMESTAMP, Value_temp = Value)
 soil_vwc <- rename( soil_vwc, TimeStamp_vwc = TIMESTAMP, Value_vwc = Value)
-df |> left_join(soil_vwc, by = "SensorID")
+soil_temp15 |> left_join(soil_vwc, by = "SensorID")
 
-smallerdf <- slice(df, 1: 33720)
+smallerdf <- slice(soil_temp15, 1: 33720)
 smallerdf |> left_join(soil_vwc, by = "Sensor_ID")
 
  # EXAMPLE
